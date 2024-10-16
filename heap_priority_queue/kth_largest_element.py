da305d0dc670c13aa0d50b88b6d28e53a2a12f1c
Derek Gliwa <derek.gliwa@gmail.com>
2024-10-15 22:54:23 -0500

    fix imports

--- heap_priority_queue/kth_largest_element.py
+++ heap_priority_queue/kth_largest_element.py
@@ -1,9 +1,12 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+import heapq
+
+
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap  = nums[:k]
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        heap = nums[:k]
         heapq.heapify(heap)
         for n in nums[k:]:
             heapq.heappushpop(heap, n)
         return heap[0]
-        
