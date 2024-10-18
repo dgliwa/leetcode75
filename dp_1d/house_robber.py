3b2e24fcd4b3d81b66ccb5e09af5fe7e3bfa4712
Derek Gliwa <derek.gliwa@gmail.com>
2024-10-18 08:51:46 -0500

    fixes

--- dp_1d/house_robber.py
+++ dp_1d/house_robber.py
@@ -6,6 +6,8 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 2:
             return max(nums)
-        max_amount = 0
+        if nums[1] < nums[0]:
+            nums[1] = nums[0]
         for i in range(2, len(nums)):
             nums[i] = max(nums[i - 2] + nums[i], nums[i - 1])
+        return nums[-1]
